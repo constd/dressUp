1440624a8a80272d4afe5c0d193f0d423ab2f306
Constantinos Dimitriou <constd@outlook.com>
2018-08-08 22:02:58 -0700

    resnet bugfix

--- dressup/resnet.py
+++ dressup/resnet.py
@@ -3,7 +3,7 @@ from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
 
-resnet = ResNet50(weights='imagenet')
+rn50 = ResNet50(weights='imagenet')
 
 
 def resnet_classifier(image_path):
@@ -11,5 +11,5 @@ def resnet_classifier(image_path):
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
-    preds = resnet.predict(x)
+    preds = rn50.predict(x)
     return [x[1] for x in decode_predictions(preds, top=2)[0]]
