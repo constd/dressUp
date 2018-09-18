bade86e0479027ea821be4966c071ba96cb5d97f
Constantinos Dimitriou <constd@outlook.com>
2018-09-18 01:25:38 -0700

    added functions that accept an image and returns the names of the classes

--- dressup/resnet.py
+++ dressup/resnet.py
@@ -15,9 +15,25 @@ def resnet_classifier(image_path):
     return set([x[1] for x in decode_predictions(preds, top=2)[0]])
 
 
-def resnet_filter(image_path, allowed_classes=set('gown', 'mosquito_net', 'hoopskirt', 'groom')):
+def resnet_filter(image_path, allowed_classes=set(['gown', 'mosquito_net', 'hoopskirt', 'groom'])):
     predictions = resnet_classifier(image_path)
     if len(allowed_classes.union(predictions)) > 0:
         return True
     else:
         return False
+
+
+def resnet_classifier_img(img):
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = rn50.predict(x)
+    return set([x[1] for x in decode_predictions(preds, top=3)[0]])
+
+
+def resnet_filter_img(img, allowed_classes=set(['gown', 'mosquito_net', 'hoopskirt', 'groom'])):
+    predictions = resnet_classifier_img(img)
+    if len(allowed_classes.intersection(predictions)) > 0:
+        return " | ".join(list(predictions))
+    else:
+        return "nope"
